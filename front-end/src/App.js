9072438a7659bcb8e7d06abd88012e8e9a5e5850
Miguel Marquez <miguel532013@gmail.com>
2018-05-11 23:32:56 -0500

    Add heading and form for teach me senpai

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -6,13 +6,37 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1>Find A Senpai!</h1>
+        <form>
+          <p>Would you like to teach or learn?</p>
+          <select name="teachOrLearn">
+            <option value="Select" selected>- Select -</option>
+            <option value="English">English</option>
+            <option value="Spanish">Spanish</option>
+            <option value="JavaScript">JavaScript</option>
+            <option value="MuicTheory">Music Theory</option>
+          </select>
+          <br />
+          <p>Would you like to teach or learn?</p>
+          <select name="subject">
+            <option value="Select" selected>- Select -</option>
+            <option value="English">English</option>
+            <option value="Spanish">Spanish</option>
+            <option value="JavaScript">JavaScript</option>
+            <option value="MuicTheory">Music Theory</option>
+          </select>
+          <br />
+          <p>Your Email Address</p>
+          <input type="text" name="emailAddress" />
+          <br />
+          <br />
+          <input type="submit" value="submit"/>
+          <br />
+        </form>
+        <p>Find or become a senpai, a Japanese word broadly used for teacher.</p>
+        <p>"Find A Senpai!" connects you with someone who will teach you or will be open to learn from you!</p>
+        <p>When your student or Senpai is ready for you, you will receive an email with their email. From there the 
+          Senpai will lead the student through lessons via a communication meidum that is agreed upon.</p>
       </div>
     );
   }
